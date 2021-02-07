d277ab64836b22bd1ec63e8117b68635649d45f2
Richard Schneider <richard@schneiderbox.net>
2021-02-07 16:27:28 -0600

    Start working on deriving state information. We'll expand this as we add more fields.

--- state.c
+++ state.c
@@ -10,7 +10,7 @@ void State_randomStart(struct State *state) {
     
     // Randomize the starting squares by interating through all squares
     // and picking a random location for each. The vacant square will
-    // be at the last location
+    // be at the last location.
     for (enum Resource resource = 0; resource < NUM_RESOURCES; resource++) {
         for (int limit = 1; limit <= MAX_LIMIT; limit ++){
             // Find a place that does not already have a square. All
@@ -23,5 +23,32 @@ void State_randomStart(struct State *state) {
             state->squares[try_place].resource = resource;
             state->squares[try_place].limit = limit;
         }
+    }
+}
+
+
+void State_derive(struct State *state) {
+    // Derive nodeCount
+    memset(state->nodeCount, 0, sizeof(state->nodeCount)*NUM_PLAYERS);
+    for (int i = 0; i < NUM_CORNERS; i++) {
+        uint_fast32_t bit = 1lu << i;
+        for(enum Player player = 0; player < NUM_PLAYERS; player++) {
+            if (bit & state->nodes[player]) {
+                state->nodeCount[player] += 1;
+            }
+        }
+    }
+
+    // Derive scores
+    // TODO Account for captured squares
+    // TODO Account for largest network
+    for (enum Player player = 0; player < NUM_PLAYERS; player++) {
+        state->scores[player] = state->nodeCount[player];
+    }
+
+    // Derive phase
+    state->phase = PLACE;
+    if (state->nodeCount[PLAYER_1] >= 2){
+        state->phase = PLAY;
     }
 }
--- state.h
+++ state.h
@@ -9,12 +9,11 @@
 #define NUM_SQUARES (3*4 + 1)
 #define NUM_CORNERS 24
 #define NUM_EDGES 36
-
-typedef uint_fast32_t nodebits;
-typedef uint_fast64_t branchbits;
+#define START_NODES 2
 
 enum Player {PLAYER_1=0, PLAYER_2};
 enum Resource {RED=0, YELLOW, BLUE, GREEN};
+enum Phase {PLACE, PLAY};
 
 
 struct Square {
@@ -24,23 +23,30 @@ struct Square {
 
 
 struct State {
-    // Unchanging information
+    // Unchanging (per game) information
     struct Square squares[NUM_SQUARES];
 
     // Core information
-    nodebits nodes[2];
-    branchbits branches[2];
-
-    uint_fast8_t playerResources[NUM_PLAYERS][NUM_RESOURCES];
-
+    uint_fast32_t nodes[2];
+    uint_fast64_t branches[2];
+    uint_fast8_t resources[NUM_PLAYERS][NUM_RESOURCES];
     enum Player turn;
 
     // Derived information
-    uint_fast8_t playerScores[NUM_PLAYERS];   
+    uint_fast8_t nodeCount[NUM_PLAYERS];
+    uint_fast8_t scores[NUM_PLAYERS];
+    enum Phase phase;
 };
 
 
+// Start a game, with a standard random layout
 void State_randomStart(struct State *state);
+
+// Calculate the derived information from code information. Useful when
+// loading a state or making changes outside of normal operations.
+void State_derive(struct State *state);
+
+// Print state to stderr
 void State_print(const struct State *state);
 
 #endif
