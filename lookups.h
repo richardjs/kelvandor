83c2811be4de89a37a0f42c0fb8ef568a32e400f
Richard Schneider <richard@schneiderbox.net>
2021-02-16 21:02:20 -0600

    Add EDGE_ADJACENT_CORNERS to lookups.h

--- lookups.h
+++ lookups.h
@@ -11,6 +11,7 @@ extern const int SQUARE_ADJACENT_SQUARES[NUM_SQUARES][4];
 extern const uint_fast64_t SQUARE_ADJACENT_BRANCHES[NUM_SQUARES][4];
 extern const int EDGE_ADJACENT_SQUARES[NUM_EDGES][2];
 extern const uint_fast64_t EDGE_ADJACENT_EDGES[NUM_EDGES];
+extern const uint_fast32_t EDGE_ADJACENT_CORNERS[NUM_EDGES];
 
 
 #endif
