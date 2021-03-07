80afcdb25f9336a2cabfcbc492721da7be90df88
Richard Schneider <richard@schneiderbox.net>
2021-03-07 13:27:17 -0600

    Add httpapi

---
+++ src/httpapi.py
@@ -0,0 +1,24 @@
+import json
+from subprocess import Popen, PIPE
+
+from flask import Flask
+
+
+KELVANDOR = './kelvandor'
+
+
+app = Flask(__name__)
+
+
+@app.route('/<board>')
+def think(board):
+    p = Popen((KELVANDOR, board), stdout=PIPE, stderr=PIPE)
+
+    actions = []
+    for action in p.stdout.readlines():
+        actions.append(action.strip().decode('utf-8'))
+
+    return json.dumps({
+        'actions': actions,
+        'log': p.stderr.read().decode('utf-8'),
+    })
