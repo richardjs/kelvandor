1ad3ba00420d97e2391d9c7991469518da9eb5f5
Richard Schneider <richard@schneiderbox.net>
2021-03-21 21:05:31 -0500

    Implement Mode header in httpapi and connect the 'move scan' mode to the -m flag

--- src/httpapi.py
+++ src/httpapi.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 def options(board):
     response = Response()
     response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Headers'] = 'Iterations'
+    response.headers['Access-Control-Allow-Headers'] = 'Iterations, Mode'
 
     return response
 
@@ -25,6 +25,7 @@ def think(board):
     assert board.isalnum()
 
     args = []
+    mode = 'normal'
     if 'Iterations' in request.headers:
         try:
             args += ['-i', str(
@@ -34,7 +35,22 @@ def think(board):
         except ValueError:
             pass
 
+    if 'Mode' in request.headers:
+        if request.headers['Mode'] == 'move scan':
+            args += ['-m']
+            mode = 'move scan'
+
     p = Popen([KELVANDOR, board] + args, stdout=PIPE, stderr=PIPE)
+
+    if mode == 'move scan':
+        actions = []
+        for line in p.stdout.readlines():
+            action, value = line.strip().decode('utf-8').split('\t')
+            actions.append({'action': action, 'value': value})
+        response = jsonify(actions)
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Headers'] = 'Iterations, Move'
+        return response
 
     actions = []
     for action in p.stdout.readlines():
@@ -51,6 +67,6 @@ def think(board):
     })
 
     response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Headers'] = 'Iterations'
+    response.headers['Access-Control-Allow-Headers'] = 'Iterations, Move'
 
     return response
